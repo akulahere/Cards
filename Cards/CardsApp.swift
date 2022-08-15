5c90abe0e02df34ff71cfaa37097003a8a0fe2d5
akula <akulinind@gmail.com>
2022-08-15 11:34:36 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Dmytro Akulinin on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
